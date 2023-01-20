d46192a0b33e036f56549aa11228512e69ae107b
dev <dev@Dev-Pro.local>
2023-01-20 18:48:11 +0500

    Added Features section

--- src/Components/Features.tsx
+++ src/Components/Features.tsx
@@ -1,11 +1,33 @@
-import { Box, Image, Text } from "@chakra-ui/react"
+import { HassleFreeIcon, MoneyBackIcon, SubscriptionIcon } from "@/Icons/Icons";
+import { Box, HStack, Icon, Image, StackProps, Text } from "@chakra-ui/react";
+import React from "react";
 
-function Features() {
-    return (
-        <Box as="section" display="flex" justifyContent="center" pt={252}>
-            Features
-        </Box>
-    )
+interface FeatureProps extends StackProps {
+  icon: ElementType;
 }
 
-export default Features
+function Feature(props: FeatureProps) {
+  const { icon, children, ...rest } = props;
+  return (
+    <HStack {...rest} spacing="24px">
+      <Icon as={icon} boxSize="48px" />
+      <Text fontSize={"18px"} fontWeight="700">
+        {children}
+      </Text>
+    </HStack>
+  );
+}
+
+export default function Features() {
+  return (
+    <Box maxW={"1024px"} m="auto" pt="60px" pb="32px">
+      <HStack>
+        <Feature icon={MoneyBackIcon}>30 days money back Gurantee</Feature>
+        <Feature icon={HassleFreeIcon}>No setup fees 100% hassle-free</Feature>
+        <Feature icon={SubscriptionIcon}>
+          No monthly subscription Pay once and for all
+        </Feature>
+      </HStack>
+    </Box>
+  );
+}
